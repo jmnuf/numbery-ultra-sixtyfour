6c015dc32f129678c75a1ec0dadc3287d35a339f
Jose Marco Nufio Garrido <jose.m.nufio.0@gmail.com>
2024-02-16 02:06:01 +0100

    Add info and err print functions

--- src/nus64.erl
+++ src/nus64.erl
@@ -1,18 +1,23 @@
 -module(nus64).
 -export([main/0]).
 
+err(Message, Format) ->
+	io:format(standard_error, string:concat("[ERROR] ", Message), Format).
+info(Message, Format) ->
+	io:format(standard_io, string:concat("[INFO] ", Message), Format).
+
 main() ->
 	io:format("Hello world!~n", []),
 	Args = init:get_plain_arguments(),
 	case length(Args) of
 		0 ->
-			io:format("ERROR: No arguments provided, expected list of folder names to create~n");
+			err("No arguments provided, expected list of folder names to create~n", []);
 		_ ->
 			case file:get_cwd() of
 				{ok, CWD} ->
 					make_directories(CWD, 1, Args);
 				{error, Reason} ->
-					io:format("[ERROR] Failed to read current directory: ~p~n", [Reason])
+					err("Failed to read current directory: ~p~n", [Reason])
 			end
 	end,
 	init:stop().
@@ -37,19 +42,18 @@ make_directories(_, _, []) ->
 	ok; % No more arguments are left
 make_directories(CWD, Count, [DirName | Rest]) when is_integer(Count) ->
 	PaddedNumber = space_to_zero("", [[X] || X <- io_lib:format("~3.B", [Count])]),
-	io:format("Number: ~s~n", [PaddedNumber]),
 	FolderName = str_concat(PaddedNumber, ["-", DirName]),
 	DirPath = filename:join([CWD, FolderName]),
 	case filelib:ensure_dir(DirPath) of
 		ok ->
 			case file:make_dir(DirPath) of
 				ok ->
-					io:format("[INFO] Created Folder: ~s~n", [FolderName]);
+					info("Created Folder: ~s~n", [FolderName]);
 				{_, Reason} ->
-					io:format("[ERROR] Failed to create folder ~s: ~p~n", [DirPath, Reason])
+					err("Failed to create folder ~s: ~p~n", [DirPath, Reason])
 			end;
 		{error, Reason} ->
-			io:format("[ERROR] Failed to create ensure directory for ~s: ~p~n", [FolderName, Reason])
+			err("Failed to create ensure directory for ~s: ~p~n", [FolderName, Reason])
 	end,
 	make_directories(CWD, Count + 1, Rest).
 
